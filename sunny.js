c804582f51568dc99b5242f4158108772e5713d5
Sunny <155150793+sandeep-310@users.noreply.github.com>
2023-12-29 23:00:04 +0530

    finalised

--- sunny.js
+++ sunny.js
@@ -1,6 +1,18 @@
 let ham = document.querySelector(".hamburger")
 let menu = document.querySelector(".menu")
+let bar = document.querySelectorAll(".bar")
 ham.addEventListener("click", ()=>{
     menu.classList.toggle("active")
+    bar[0].classList.toggle("upperBar")
+    bar[1].classList.toggle("middleBar")
+    bar[2].classList.toggle("lowerBar")
 })
-
+let menuObj = document.querySelectorAll(".menu")
+menuObj.forEach(element => {
+    element.addEventListener("click", ()=>{
+        menu.classList.remove("active")
+        bar[0].classList.toggle("upperBar")
+        bar[1].classList.toggle("middleBar")
+        bar[2].classList.toggle("lowerBar")
+    })
+});
